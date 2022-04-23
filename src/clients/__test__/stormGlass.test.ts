4acc59bddb6b7172c8cd504640c4a96b81dbe2f8
Alysson-Araujo <alysson10araujo@gmail.com>
2022-04-23 17:42:30 -0300

    Criação da API Client para usar a API externa da meteorologia e normalizar os dados vindos dessa comunicação

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,17 +1,19 @@
 import { StormGlass } from '@src/clients/StormGlass';
-import axios from 'axios'
-import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json'
-import stormGlassNormalizedResponse3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json'
+import axios from 'axios';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalizedResponse3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
 
-jest.mock('axios')
+jest.mock('axios');
 
 describe('StormGlass client', () => {
   it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792132;
     const lng = 182.213414;
 
-    axios.get = jest.fn().mockResolvedValue({data:stormGlassWeather3HoursFixture})
-    
+    axios.get = jest
+      .fn()
+      .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+
     const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual(stormGlassNormalizedResponse3HoursFixture);
